dbcc38e623a2cdbd5d78399759980abd3bfffc3c
tflores4 <tflores4@myseneca.ca>
2023-09-02 21:55:32 -0400

    rename

---
+++ MS1/Milestone/Utils.cpp
@@ -0,0 +1,30 @@
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+// Name : Thiago Flores
+// Student ID : 150237220
+// OOP244NAA
+
+#include <iostream>
+#include "Utils.h"
+
+using namespace std;
+namespace sdds {
+	int correctInput(int minValue, int maxValue) {
+		int userInput;
+		bool correctInput = false;
+
+		while (!correctInput) {
+			cin >> userInput;
+
+			if (cin.fail() || userInput < minValue || userInput > maxValue) {
+				cout << "Invalid Selection, try again: ";
+				cin.clear();
+				cin.ignore(1000, '\n');
+			}
+			else {
+				correctInput = true;
+			}
+		}
+
+		return userInput;
+	}
+}
